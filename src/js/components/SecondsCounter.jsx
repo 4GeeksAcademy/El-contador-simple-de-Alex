9e9363619375d5f35ff317b06917678a8d3103ab
alexrolfo <131758342+alexrolfo@users.noreply.github.com>
2026-01-29 20:14:29 +0000

    Mi pequeño contador

---
+++ src/js/components/SecondsCounter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const SecondsCounter = (props) => {
+  const padded = String(props.seconds).padStart(6, "0");
+  const digits = padded.split(""); // ["0","0","0","0","4","2"]
+
+  return (
+    <div className="d-flex justify-content-center mt-4">
+      <div className="d-flex align-items-center bg-dark text-white p-3 rounded gap-2">
+
+        <div className="px-3 fs-2">
+          <i className="far fa-clock"></i>
+        </div>
+
+        {digits.map((d, i) => (
+          <div
+            key={i}
+            className="bg-secondary rounded px-3 py-2 fs-2 text-center"
+            style={{ minWidth: "55px" }}
+          >
+            {d}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SecondsCounter;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import SecondsCounter from "./components/SecondsCounter.jsx";
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,11 +9,21 @@ import "bootstrap"
 // index.css'
 import '../styles/index.css'
 
-// components
-import Home from './components/Home';
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+let seconds = 0;
+
+const renderCounter = () => {
+  root.render(
+      <React.StrictMode>
+        <SecondsCounter seconds={seconds} />
+      </React.StrictMode>
+  );
+};
+
+renderCounter();
+
+setInterval(() => {
+  seconds++;
+  renderCounter();
+}, 1000);
